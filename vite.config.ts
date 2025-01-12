e22500e34e98580d39e756e1e668934f80c159f6
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2025-01-12 13:48:41 -0600

    add deployment script and update configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import rawPlugin from 'vite-raw-plugin';
 
 export default {
+  base: '/leaf-js/', // Replace 'leaf-js' with your repository name
   plugins: [
     rawPlugin({
       fileRegex: /\.wgsl$/,
